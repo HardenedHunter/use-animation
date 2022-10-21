ce08b07b655c5139197f5d0cc250fc7a93fbddc6
Artyom Khudyakov <khudyakov@redcollar.ru>
2022-10-21 13:38:37 +0300

    TabSize refactoring

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,156 +1,156 @@
 import create from "zustand";
 
 const buildSpriteArray = (start: number, end: number) => {
-  const result = [];
-  for (let i = start; i <= end; i++) {
-    result.push(`witch/witch${i}.svg`);
-  }
+    const result = [];
+    for (let i = start; i <= end; i++) {
+        result.push(`witch/witch${i}.svg`);
+    }
 
-  return result;
+    return result;
 };
 
 const animations = {
-  idle: buildSpriteArray(1, 6),
-  run: buildSpriteArray(7, 14),
-  charge: buildSpriteArray(15, 19),
-  attack: buildSpriteArray(20, 28),
-  death: buildSpriteArray(29, 43),
-  resurrect: buildSpriteArray(29, 43).reverse(),
+    idle: buildSpriteArray(1, 6),
+    run: buildSpriteArray(7, 14),
+    charge: buildSpriteArray(15, 19),
+    attack: buildSpriteArray(20, 28),
+    death: buildSpriteArray(29, 43),
+    resurrect: buildSpriteArray(29, 43).reverse(),
 };
 
 type AnimationConfig = {
-  repeat?: boolean;
-  next?: AnimationConfig;
-  sprites: string[];
+    repeat?: boolean;
+    next?: AnimationConfig;
+    sprites: string[];
 };
 
 type CharacterModel = {
-  sprite: string;
-  playAnimation: (config: AnimationConfig) => void;
-  animation?: {
-    nextIndex: number;
-    timeoutId?: NodeJS.Timer;
-  };
+    sprite: string;
+    playAnimation: (config: AnimationConfig) => void;
+    animation?: {
+        nextIndex: number;
+        timeoutId?: NodeJS.Timer;
+    };
 };
 
 type BattleStore = {
-  left: CharacterModel;
-  right: CharacterModel;
-  animate: VoidFunction;
+    left: CharacterModel;
+    right: CharacterModel;
+    animate: VoidFunction;
 };
 
 const iterateAnimation = (
-  getCharacter: () => CharacterModel,
-  setCharacter: (character: CharacterModel) => void,
-  config: AnimationConfig
+    getCharacter: () => CharacterModel,
+    setCharacter: (character: CharacterModel) => void,
+    config: AnimationConfig
 ) => {
-  const latestCharacterData = getCharacter();
-  const { animation: latestAnimation } = latestCharacterData;
-  const { sprites, repeat, next } = config;
+    const latestCharacterData = getCharacter();
+    const { animation: latestAnimation } = latestCharacterData;
+    const { sprites, repeat, next } = config;
 
-  if (!latestAnimation) {
-    return;
-  }
+    if (!latestAnimation) {
+        return;
+    }
 
-  const sprite = sprites[latestAnimation.nextIndex] as string;
-  const nextIndex = (latestAnimation.nextIndex + 1) % sprites.length;
+    const sprite = sprites[latestAnimation.nextIndex] as string;
+    const nextIndex = (latestAnimation.nextIndex + 1) % sprites.length;
 
-  const newCharacterData = {
-    ...latestCharacterData,
-    sprite,
-    animation: { nextIndex },
-  };
+    const newCharacterData = {
+        ...latestCharacterData,
+        sprite,
+        animation: { nextIndex },
+    };
 
-  setCharacter(newCharacterData);
+    setCharacter(newCharacterData);
 
-  const isLastSprite = latestAnimation.nextIndex === sprites.length - 1;
+    const isLastSprite = latestAnimation.nextIndex === sprites.length - 1;
 
-  if (isLastSprite) {
-    if (next) {
-      setTimeout(
-        () => playAnimationInner(getCharacter, setCharacter, next),
+    if (isLastSprite) {
+        if (next) {
+            setTimeout(
+                () => playAnimationInner(getCharacter, setCharacter, next),
+                100
+            );
+
+            return;
+        }
+
+        if (!repeat) {
+            return;
+        }
+    }
+
+    const timeoutId = setTimeout(
+        () => iterateAnimation(getCharacter, setCharacter, config),
         100
-      );
+    );
 
-      return;
-    }
-
-    if (!repeat) {
-      return;
-    }
-  }
-
-  const timeoutId = setTimeout(
-    () => iterateAnimation(getCharacter, setCharacter, config),
-    100
-  );
-
-  setCharacter({
-    ...newCharacterData,
-    animation: { nextIndex, timeoutId },
-  });
+    setCharacter({
+        ...newCharacterData,
+        animation: { nextIndex, timeoutId },
+    });
 };
 
 const playAnimationInner = (
-  getCharacter: () => CharacterModel,
-  setCharacter: (character: CharacterModel) => void,
-  config: AnimationConfig
+    getCharacter: () => CharacterModel,
+    setCharacter: (character: CharacterModel) => void,
+    config: AnimationConfig
 ) => {
-  const character = getCharacter();
-  const { animation } = character;
+    const character = getCharacter();
+    const { animation } = character;
 
-  if (animation && animation.timeoutId) {
-    clearTimeout(animation.timeoutId);
-  }
+    if (animation && animation.timeoutId) {
+        clearTimeout(animation.timeoutId);
+    }
 
-  setCharacter({
-    ...character,
-    animation: { nextIndex: 0 },
-  });
+    setCharacter({
+        ...character,
+        animation: { nextIndex: 0 },
+    });
 
-  iterateAnimation(getCharacter, setCharacter, config);
+    iterateAnimation(getCharacter, setCharacter, config);
 };
 
 const useBattleStore = create<BattleStore>((set, get) => ({
-  left: {
-    sprite: "witch/witch1.svg",
-    playAnimation: (config) =>
-      playAnimationInner(
-        () => get().left,
-        (character) => set({ left: character }),
-        config
-      ),
-  },
-  right: {
-    sprite: "witch/witch1.svg",
-    playAnimation: (config) =>
-      playAnimationInner(
-        () => get().right,
-        (character) => set({ right: character }),
-        config
-      ),
-  },
-  animate: () => {
-    const { left, right } = get();
+    left: {
+        sprite: "witch/witch1.svg",
+        playAnimation: (config) =>
+            playAnimationInner(
+                () => get().left,
+                (character) => set({ left: character }),
+                config
+            ),
+    },
+    right: {
+        sprite: "witch/witch1.svg",
+        playAnimation: (config) =>
+            playAnimationInner(
+                () => get().right,
+                (character) => set({ right: character }),
+                config
+            ),
+    },
+    animate: () => {
+        const { left, right } = get();
 
-    left.playAnimation({
-      sprites: animations.attack,
-      next: { sprites: animations.idle, repeat: true },
-    });
+        left.playAnimation({
+            sprites: animations.attack,
+            next: { sprites: animations.idle, repeat: true },
+        });
 
-    const death: AnimationConfig = {
-      sprites: animations.death,
-    };
+        const death: AnimationConfig = {
+            sprites: animations.death,
+        };
 
-    const resurrect: AnimationConfig = {
-      sprites: animations.resurrect,
-      next: death,
-    };
+        const resurrect: AnimationConfig = {
+            sprites: animations.resurrect,
+            next: death,
+        };
 
-    death.next = resurrect;
+        death.next = resurrect;
 
-    right.playAnimation(death);
-  },
+        right.playAnimation(death);
+    },
 }));
 
 export default useBattleStore;
